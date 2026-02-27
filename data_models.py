63a746d393cc95da575621f290060191173bc5a8
andrewmswenson23 <andrewmswenson23@gmail.com>
2026-02-27 13:20:13 -0700

    Update data_models.py

--- data_models.py
+++ data_models.py
@@ -15,9 +15,31 @@ schedule_toxic = [
     {"id": "T5", "duration": 1, "predecessors": ["T3"]}
 ]
 
-# ... add your other schedules (broken, complex) here ...
+schedule_broken = [
+    {"id": "Project_Kickoff", "duration": 2, "predecessors": []},
+    {"id": "Geotech_Testing", "duration": 10, "predecessors": ["Project_Kickoff"]},
+    {"id": "Structural_Drafting", "duration": 15, "predecessors": ["Geotech_Testing"]},
+    {"id": "Zoning_Review", "duration": 20, "predecessors": ["Project_Kickoff"]},
+    {"id": "Foundation_Pour", "duration": 10, "predecessors": ["Structural_Drafting", "Zoning_Review"]},
+    {"id": "Superstructure", "duration": 25, "predecessors": ["Foundation_Pour"]},
+    {"id": "Interior_Fitout", "duration": 20, "predecessors": ["Superstructure"]},
+    {"id": "Final_Handover", "duration": 0, "predecessors": ["Interior_Fitout"]}
+]
+
+schedule_complex = [
+    {"id": "Permitting", "duration": 30, "predecessors": []},
+    {"id": "Site_Survey", "duration": 10, "predecessors": ["Permitting"]},
+    {"id": "Steel_Procurement", "duration": 45, "predecessors": ["Permitting"]},
+    {"id": "Excavation", "duration": 20, "predecessors": ["Site_Survey"]},
+    {"id": "Concrete_Pour", "duration": 10, "predecessors": ["Excavation"]},
+    {"id": "Steel_Erection", "duration": 25, "predecessors": ["Steel_Procurement", "Concrete_Pour"]},
+    {"id": "Interior_Walls", "duration": 12, "predecessors": ["Steel_Erection"]},
+    {"id": "Project_Closeout", "duration": 2, "predecessors": ["Interior_Walls"]}
+]
 
 schedulesdb = {
     "schedulehealthy": schedule_healthy,
     "scheduletoxic": schedule_toxic,
+    "schedulebroken": schedule_broken,
+    "schedulecomplex": schedule_complex
 }
